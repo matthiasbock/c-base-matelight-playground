caca4f6d92a0cd75f63eb41416b0e195a7da2a36
Matthias Bock <mail@matthiasbock.net>
2019-03-07 23:34:16 +0100

    Something more

--- mrbombastic.py
+++ mrbombastic.py
@@ -1,30 +1,49 @@
 #!/usr/bin/python3
 
 from random import *
+from subprocess import Popen, PIPE, run
+from shlex import split
 
 LENGTH=640
 PADDING=4
+HOST="api.matelight.rocks"
+PORT=1337
 
 def getRandom():
-    return int(random()*256)
+    return int(random()*256).to_bytes(1, byteorder="big")
 
-def getColor():
+def getRandomColor():
     r = getRandom()
     g = getRandom()
     b = getRandom()
     return (r,g,b)
 
 def generateRandomSequence():
-    sequence = ""
+    sequence = b""
     for i in range(LENGTH):
-        c = getColor()
-        sequence += chr(c[0]) + chr(c[1]) + chr(c[2])
+        c = getRandomColor()
+        sequence += c[0] + c[1] + c[2]
     for i in range(PADDING):
-        sequence += chr(0)
+        sequence += b"\0"
     return sequence
 
 #if __name__ == "__main__":
 seed()
 
-print(generateRandomSequence())
+data = generateRandomSequence()
+print("Sequence length is {:d}".format(len(data)))
+
+f=open("datei","wb")
+f.write(data)
+f.close()
+
+cmd = split("netcat -u {:s} {:d}".format(HOST, PORT))
+p = Popen(cmd, stdin=PIPE)
+p.communicate(input = data)
+p.terminate()
+#p.wait()
+
+#run(cmd, stdout=PIPE, input=generateRandomSequence(), encoding="utf-8")
+
+#print(generateRandomSequence())
 
